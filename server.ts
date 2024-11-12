cc873b2db01a67e77bb030256d1a0485acb2e2ff
cr9169 <bar.gabay.home@gmail.com>
2024-11-12 22:51:56 +0200

    feat: finished server, made IUser, checked routes

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import winston from 'winston';
 import { Redis } from 'ioredis';
+import IUser from './user';
 
+const port = 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,3 +15,94 @@ const redisClient = new Redis({
   password: '', // Leave empty if no password set
   db: 0, // Default database index
 });
+
+const setUserDetailsWithId = async (id: string, userDetails: IUser) => {
+  const status = await redisClient.set(
+    `user:${id}`,
+    JSON.stringify(userDetails),
+  );
+  return status;
+};
+
+// using set func
+const setUserDetailsWithIdAndEx1 = async (
+  id: string,
+  userDetails: IUser,
+  exTimeInSeconds: number,
+) => {
+  const status = await redisClient.set(
+    `user:${id}`,
+    JSON.stringify(userDetails),
+    'EX',
+    exTimeInSeconds,
+  );
+  return status;
+};
+
+// using setex func
+const setUserDetailsWithIdAndEx2 = async (
+  id: string,
+  userDetails: IUser,
+  exTimeInSeconds: number,
+) => {
+  const status = await redisClient.setex(
+    `user:${id}`,
+    exTimeInSeconds,
+    JSON.stringify(userDetails),
+  );
+  return status;
+};
+
+const getUserById = async (id: string) => {
+  const user = await redisClient.get(`user:${id}`);
+  return user;
+};
+
+const getUserByIdAndModifyExTime = async (
+  id: string,
+  exTimeInSeconds: number,
+) => {
+  const user = await redisClient.getex(`user:${id}`, 'EX', exTimeInSeconds);
+  return user;
+};
+
+app.get(
+  '/users/:id',
+  async (req: Request, res: Response, _next: NextFunction) => {
+    const user = await getUserById(req.params.id);
+    res.json(user ?? 'Cant find user :(');
+  },
+);
+app.patch(
+  '/users/:id/ttl/:ttl',
+  async (req: Request, res: Response, _next: NextFunction) => {
+    const user = await getUserByIdAndModifyExTime(
+      req.params.id,
+      Number(req.params.ttl),
+    );
+    res.json(user ?? 'Cant find user :(');
+  },
+);
+
+//
+app.post(
+  '/users/:id',
+  async (req: Request, res: Response, _next: NextFunction) => {
+    const status = await setUserDetailsWithId(req.params.id, req.body);
+    res.json(status ?? 'Data Posting Failed failed :(');
+  },
+);
+
+app.post(
+  '/users/:id/ttl/:ttl',
+  async (req: Request, res: Response, _next: NextFunction) => {
+    const status = await setUserDetailsWithIdAndEx1(
+      req.params.id,
+      req.body,
+      Number(req.params.ttl),
+    );
+    res.json(status ?? 'Data Posting Failed failed :(');
+  },
+);
+
+app.listen(port, () => console.log(`Started server on port ${port}`));
